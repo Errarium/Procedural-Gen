69150de829c360798a3cd01cd2f27c0996cffcad
Erra <erling.blomberg@proton.me>
2024-01-30 01:57:49 +0100

    Initial commit

---
+++ Assets/Scripts/vsc.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vsc : MonoBehaviour
+{
+    [SerializeField] private int myint = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myint = 9;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
